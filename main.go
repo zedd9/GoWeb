c45c2202c023cfb1fae6503e1f26e805fdb92a0e
Zedd <gusdnre@gmail.com>
2020-05-16 22:43:55 +0900

    [Web] Http 서버 시작 + mux 사용

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type fooHandler struct{}
+
+func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello Foo!")
+}
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello Bar!")
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	})
+
+	mux.HandleFunc("/bar", barHandler)
+
+	mux.Handle("/foo", &fooHandler{})
+
+	http.ListenAndServe(":3000", mux)
+}
