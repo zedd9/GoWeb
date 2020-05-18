e8c702e949d131fa3f54777d8942523bc2bc116a
Zedd <gusdnre@gmail.com>
2020-05-18 23:24:20 +0900

    [WEB] 자동 Test 환경 구축 테스트

--- main.go
+++ main.go
@@ -1,56 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"time"
+
+	"github.com/zedd9/GoWeb/myapp"
 )
 
-type User struct {
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Email     string    `json:"email"`
-	CreateAt  time.Time `json:"created_at"`
-}
-
-type fooHandler struct{}
-
-func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	user := new(User)
-	err := json.NewDecoder(r.Body).Decode(user)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "Bad Request: ", err)
-		return
-	}
-
-	user.CreateAt = time.Now()
-
-	data, err := json.Marshal(user)
-	w.Header().Add("content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, string(data))
-}
-func barHandler(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if name == "" {
-		name = "World"
-	}
-
-	fmt.Fprintf(w, "Hello %s!", name)
-}
-
 func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World")
-	})
-
-	mux.HandleFunc("/bar", barHandler)
-
-	mux.Handle("/foo", &fooHandler{})
-
-	http.ListenAndServe(":3000", mux)
+	http.ListenAndServe(":3000", myapp.NewHttpHandler())
 }
